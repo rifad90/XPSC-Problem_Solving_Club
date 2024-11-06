4b4fdf7df6a69c82c998fa4996aa8dd401500187
Rifadul Islam <rifadulislam@gmail.com>
2024-11-07 00:04:19 +0600

    Add files via upload

---
+++ XPSC/Week1/Day5/ICECONE.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int c,s;
+    cin>>c>>s;
+    cout<<min(c,s)<<endl;
+    return 0;
+}
