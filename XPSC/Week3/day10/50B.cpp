5d8384284c13f89caf8d0a3c8d6eac936b24fa4f
Rifadul Islam <rifadulislam@gmail.com>
2024-11-29 12:42:16 +0600

    Add files via upload

---
+++ XPSC/Week3/day10/50B.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    cin >> n;
+    map<string, string> ans, has;
+    while (n--)
+    {
+        string a,b;
+        cin>>a>>b;
+        if(has.find(a) != has.end()){
+            string s=has[a];
+            ans[s]=b;
+            has.erase(a);
+            has[b]=s;
+        }else{
+            ans[a]=b;
+            has[b]=a;
+        }
+    }
+    cout<<ans.size()<<endl;
+    for(auto [x,y]: ans){
+        cout<<x<<" "<<y<<endl;
+    }
+
+    return 0;
+}
